04c89cb9e8f7f318636072592c54dc3fa2ee3d00
ZihanZHN <zim.zimti1@gmail.com>
2022-06-21 16:28:11 +0530

    first commit

---
+++ hire.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Bootbusiness | Short description about company">
+    <meta name="author" content="Your name">
+    <title>HAND TO YOUR DOOR STEP</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap responsive -->
+    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+    <!-- Font awesome - iconic font with IE7 support --> 
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <link href="css/font-awesome-ie7.css" rel="stylesheet">
+    <!-- Bootbusiness theme -->
+    <link href="css/boot-business.css" rel="stylesheet">
+    <link href="css/prcss.css" rel="stylesheet">
+    <style>
+      body {
+        background-image: url("img/content.png");
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover;
+      }
+		
+      body,td,th {
+    font-size: 24px;
+    color: #CF292B;
+}
+    </style> 
+  </head>
+  <body>
+    <!-- Start: HEADER -->
+    <header>
+      <!-- Start: Navigation wrapper -->
+      <div class="navbar navbar-fixed-top">
+        <div class="navbar-inner">
+          <div class="container">
+              <a href="index.html" class="brand brand-bootbus">HAND TO YOUR DOOR STEP</a>
+              <!-- Below button used for responsive navigation -->
+                <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+                </button>
+                <!-- Start: Primary navigation -->
+                <div class="nav-collapse collapse">        
+                 <ul class="nav pull-right">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Services<b class="caret"></b></a>
+                     <ul class="dropdown-menu">
+                                 
+                        <li class="divider"></li>
+                        <li class="nav-header">SERVICES</li>
+                        <li><a href="service.html">Service1</a></li>
+                        <li><a href="service.html">Service2</a></li>
+                        <li><a href="service.html">Service3</a></li>
+                        <li><a href="all_services.html">All services</a></li>
+                     </ul>                  
+                    </li>
+               
+                    <li><a href="faq.html">Feedback</a></li>
+                    <li><a href="contact_us.html">Contact us</a></li>
+                    <li><a href="signup.html" >Sign up</a></li>
+                    <li><a href="signin.html">Sign in</a></li>
+                 </ul>
+                </div>
+          </div>
+        </div>
+      </div>
+      <!-- End: Navigation wrapper -->   
+    </header>
+    <!-- End: HEADER -->
+    <table border="0">
+        <tr><td><h1>   </h1>  </td></tr>
+        <tr><td><h1>   </h1>    </td></tr>
+        <tr><td><h1>   </h1>   <br>
+            <br> 
+            <br>
+            <br>
+            <br>
+        </td></tr>
+        <tr><td> <h1 align="center">
+            You have successfully hired the Service Provider.</h1> </td></tr>
+            <tr><td><h1>   </h1>  </td></tr>
+        <tr><td><h1>   </h1>    </td></tr>
+        <tr><td><h1>   </h1>   <br>
+            <br> 
+            <br>
+            <br>
+            <br>
+        </td></tr>
+        <tr><td><h1>   </h1>  </td></tr>
+        <tr><td><h1>   </h1>    </td></tr>
+        <tr><td><h1>   </h1>   <br>
+            <br> 
+            <br>
+            <br>
+            <br>
+        </td></tr>
+	 
+</table>
+    <!-- Start: FOOTER -->
+    <footer>
+        <div class="container">
+          <div class="row">
+            <div class="span2">
+              
+              <h4><i class="icon-cogs icon-white"></i> Services</h4>
+              <nav>
+                <ul class="quick-links">
+                  <li><a href="service.html">Service1</a></li>
+                  <li><a href="service.html">Service2</a></li>
+                  <li><a href="service.html">Service3</a></li>
+                  <li><a href="all_services.html">All services</a></li>              
+                </ul>
+              </nav>
+            </div>
+           
+            <div class="span2">
+              <h4><i class="icon-thumbs-up icon-white"></i> Support</h4>
+              <nav>
+                <ul class="quick-links">
+                  <li><a href="faq.html">FAQ</a></li>
+                  <li><a href="contact_us.html">Contact us</a></li>            
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+  </footer>
+      <!-- End: FOOTER -->
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/boot-business.js"></script>
+  <script type="text/javascript" src="js/prjs.js"></script>
+</body>
+  </html>
